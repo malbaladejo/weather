b958467fbc00d4cb249577897ae7811d3e92168d
malbaladejo <malbaladejo@free.fr>
2022-12-23 09:39:18 +0100

    temp dashboard

---
+++ WeatherApp/WeatherApp/Controllers/TemperatureController.cs
@@ -0,0 +1,12 @@
+﻿using WeatherApp.ViewModels;
+
+namespace WeatherApp.Controllers
+{
+    public class TemperatureController : ControllerBase
+    {
+        public TemperatureController(IWeatherDataViewModelFactory weatherDataViewModelFactory)
+            : base(ControllerNames.Temperature, weatherDataViewModelFactory)
+        {
+        }
+    }
+}
