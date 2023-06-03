3d99b8bafc9d5ded15a1ff1d26296e7e8c6ac9fa
malbaladejo <malbaladejo@free.fr>
2023-06-03 08:16:32 +0200

    v19.1.0: bug fix temperature out label

--- WeatherApp/WeatherApp/Program.cs
+++ WeatherApp/WeatherApp/Program.cs
@@ -20,10 +20,7 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
 }
-
 
 app.AddWepApplicationLocalizations("fr-FR", "en-US");
 
